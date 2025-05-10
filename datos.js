8f8ee6cbe6542d9743c19d2c58b61e397bb24a32
SebasAuM <Aasebastiann@gmail.com>
2025-05-09 23:33:37 -0500

    datos.js

---
+++ datos.js
@@ -0,0 +1,12 @@
+const personas = {
+    "001": {
+      nombre: "Juan Pérez",
+      descripcion: "Desarrollador web con 5 años de experiencia."
+    },
+    "002": {
+      nombre: "Ana Gómez",
+      descripcion: "Diseñadora gráfica especializada en branding."
+    },
+    // Puedes agregar más personas aquí
+  };
+  
