1d2ace73e75e8e24b504b98efc90e2427ded18e3
SebasAuM <Aasebastiann@gmail.com>
2025-05-09 23:42:50 -0500

    datos.js

--- datos.js
+++ datos.js
@@ -1,15 +1,29 @@
 const personas = {
     "001": {
-      nombre: "Juan Pérez",
-      descripcion: "Desarrollador web con 5 años de experiencia."
+        nombre: "Juan Pérez",
+        descripcion: "Desarrollador web con 5 años de experiencia."
     },
     "002": {
-      nombre: "Ana Gómez",
-      descripcion: "Diseñadora gráfica especializada en branding."
+        nombre: "Ana Gómez",
+        descripcion: "Diseñadora gráfica especializada en branding."
     },
     "003": {
-      nombre: "Sebastian Aurela",
-      descripcion: "Ingeniero de sistemas."
-    
-  };
-  
+        nombre: "Sebastian Aurela",
+        descripcion: "Ingeniero de sistemas."
+    },
+    "004": {
+        nombre: "Hector Enrique",
+        descripcion: "Ingeniero mecatronico."
+    },
+    "005": {
+        nombre: "David ",
+        descripcion: "Ingeniero electronico."
+    },
+    "006": {
+        nombre: "Mafe",
+        descripcion: "Ingeniera mecatronica."
+    },
+
+
+
+};
